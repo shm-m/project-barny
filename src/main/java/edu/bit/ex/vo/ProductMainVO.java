1fc9ade67bc28330ce0dcf90490e7a2fa4616563
LAPTOP-SBPJN31Q\01088 <01088287493@daum.net>
2021-07-14 13:07:33 +0900

    상품보기 기능만..!

---
+++ src/main/java/edu/bit/ex/vo/ProductMainVO.java
@@ -0,0 +1,22 @@
+package edu.bit.ex.vo;
+
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class ProductMainVO {
+    private int image_id;
+    private String product_name;
+    private int price;
+    private int category_num;
+
+}
