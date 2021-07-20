ad1d2e0845969aa20e9b31422257087e571adf36
DESKTOP-4JGFVLV\SeheeMoon <sehee0230@gmail.com>
2021-07-20 17:38:26 +0900

    탑버튼 수정, 카카오 채팅

--- src/main/resources/static/main_page/js/scripts.js
+++ src/main/resources/static/main_page/js/scripts.js
@@ -54,6 +54,7 @@ window.addEventListener('DOMContentLoaded', event => {
 });
 
 var mybutton = document.getElementById("myBtn");
+var kakao = document.getElementById("kakao-chat");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
@@ -61,10 +62,13 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         mybutton.style.display = "block";
+        kakao.style.display = "block";
     } else {
         mybutton.style.display = "none";
+        kakao.style.display = "none";
     }
 }
+
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
