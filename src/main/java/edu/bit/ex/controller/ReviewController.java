c1786da4cea72859a106b56ad4de7ab2685d34a8
yun6160 <devyun6160@gmail.com>
2021-08-06 11:27:49 +0900

    review add file

--- src/main/java/edu/bit/ex/controller/ReviewController.java
+++ src/main/java/edu/bit/ex/controller/ReviewController.java
@@ -1,6 +1,9 @@
 package edu.bit.ex.controller;
 
+import java.io.File;
+import java.io.IOException;
 import java.security.Principal;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,10 +15,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import edu.bit.ex.service.ProductMainService;
 import edu.bit.ex.service.SelectHitService;
+import edu.bit.ex.vo.FileVO;
 import edu.bit.ex.vo.ProductMainVO;
 import edu.bit.ex.vo.account.MemberContext;
 import lombok.extern.slf4j.Slf4j;
@@ -79,9 +85,37 @@ public class ReviewController {
 
     // 후기 write
     @PostMapping("/review/write")
-    public String writeReview(ProductMainVO productMainVO) {
+    public String writeReview(ProductMainVO productMainVO, FileVO fileVO,
+            @RequestParam("file") MultipartFile[] uploadFile) throws IOException {
 
         productMainService.writeReview(productMainVO);
+
+        String uploadFolder = "C:\\Users\\devyun\\Workspace\\project_barny\\src\\main\\webapp\\WEB-INF\\upload";
+
+        for (MultipartFile multipartFile : uploadFile) {
+            log.info("===================================");
+            log.info("upload File Name: " + multipartFile.getOriginalFilename());
+            log.info("upload File Size: " + multipartFile.getSize());
+
+            String fileExtension = multipartFile.getOriginalFilename()
+                    .substring(multipartFile.getOriginalFilename().lastIndexOf("."));
+            // lastIndexOf(".") = 14(index는 0번부터)
+            // substring(14) = .doc
+            String storedFileName = UUID.randomUUID().toString().replaceAll("-", "") + fileExtension;
+            // UUID클래스 - (특수문자를 포함한)문자를 랜덤으로 생성 "-"라면 생략으로 대체
+
+            log.info("upload File SaveName: " + storedFileName);
+
+            File saveFile = new File(uploadFolder, storedFileName);
+
+            try {
+                multipartFile.transferTo(saveFile);
+            } catch (Exception e) {
+                log.error(e.getMessage());
+            } // end catch
+            log.info("===================================");
+
+        }
         String redirect = "redirect:/product_view?product_id=" + productMainVO.getProduct_id();
         // http://localhost:8282/product_view?product_id=6
         return redirect; // 다이렉트로 특정 상품 리스트로 가게
