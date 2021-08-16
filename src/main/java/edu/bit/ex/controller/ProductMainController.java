961e1f234399415976a02c46cedce11405507848
yun6160 <devyun6160@gmail.com>
2021-08-16 13:36:51 +0900

    좋아요 중복 확인

--- src/main/java/edu/bit/ex/controller/ProductMainController.java
+++ src/main/java/edu/bit/ex/controller/ProductMainController.java
@@ -2,15 +2,10 @@ package edu.bit.ex.controller;
 
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import edu.bit.ex.page.Criteria;
 import edu.bit.ex.page.PageVO;
